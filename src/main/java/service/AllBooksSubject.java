367a73c8e95065ef16fa24659c5c7ca4bae97e2a
MarcoGh10 <77391323+MarcoGh10@users.noreply.github.com>
2024-01-13 14:23:17 +0200

    m

---
+++ src/main/java/service/AllBooksSubject.java
@@ -0,0 +1,24 @@
+package service;
+
+import models.Book;
+
+
+public class AllBooksSubject extends Subject {
+    @Override
+    public void registerObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void unregisterObserver(Observer observer) {
+
+    }
+
+    @Override
+    public void notifyObserver() {
+
+    }
+
+    public void add(Book book) {
+    }
+}
---
+++ src/main/java/service/Subject.java
@@ -0,0 +1,19 @@
+package service;
+
+import java.util.ArrayList;
+
+public class Subject {
+    ArrayList<Observer> observerCollection = new ArrayList<>();
+
+    void registerObserver(Observer observer) {
+
+    }
+
+    void unregisterObserver(Observer observer) {
+
+    }
+
+    void notifyObserver() {
+
+    }
+}
